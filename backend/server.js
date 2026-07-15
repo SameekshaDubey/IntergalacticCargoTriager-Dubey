5d85165fe22996feb66e128ee7de3761f077fd51
SameekshaDubey <dubeysamiksha458@gmail.com>
2026-07-15 12:21:57 +0530

    Task 2 Business Rule added

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,16 @@ const app = express();
 const cargo = require("./cargo.json");
 
 
+app.get("/api/cargo", (req, res) => {
 
+    if (req.header("X-System-Override") === "true") {
+        return res
+            .status(418)
+            .send("System override denied.");
+    }
+
+    res.json(cargo);
+});
 
 
 app.listen(3000, () => {
