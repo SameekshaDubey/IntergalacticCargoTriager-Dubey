7d1c107274a392cbbb495a58c4df015fc2615a4f
SameekshaDubey <dubeysamiksha458@gmail.com>
2026-07-15 12:03:37 +0530

    Task 1 Initial parser

---
+++ parser.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("manifest.txt", "utf8");
+const lines = data.trim().split("\n");
+
+const cargo = [];
+
+const regex = /\[(.*?)\]\s+\|\|\s+(.*?)\s+::\s+(\d+)\s+>>\s+(.*)/;
+
+function isPrime(num) {
+    if (num < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+
+    return true;
+}
+
+for (const line of lines) {
+    const match = line.match(regex);
+
+    if (!match) continue;
+
+    const date = match[1];
+    const id = match[2];
+    let weight = Number(match[3]);
+    const destination = match[4];
+
+
+    // added
+    weight = Math.round(weight);
+
+    if (isPrime(weight)) {
+        continue;
+    }
+
+    cargo.push({
+        date,
+        cargoId: id,
+        weight,
+        destination
+    });
+}
+
+// read 
